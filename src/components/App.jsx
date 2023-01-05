e1cc0bd9959754abb75f36597f63037c8ff46b06
Vasyl Horaichuk <vg.createam@gmail.com>
2023-01-05 16:56:40 +0200

    make friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
-// import data from './Data.data.json';
 import { Profile } from './Profile/Profile';
 import { StatisticList } from './StatisticList/StatisticList';
+import { FriendList } from './FriendList/FriendList';
 
 import user from './Data/user.json';
 import statistics from './Data/data.json';
+import friends from './Data/friends.json';
 
 export const App = () => {
   return (
@@ -16,6 +17,7 @@ export const App = () => {
         stats={user.stats}
       />
       <StatisticList title="Upload stats" statistics={statistics} />
+      <FriendList friends={friends} />
     </div>
   );
 };
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,1 +1,13 @@
-import { App } from "components/App";
+export const FriendList = ({ friends }) => {
+  return (
+    <ul class="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li class="friend-iteam" key={id}>
+          <span class="status">{isOnline}</span>
+          <img class="avatar" src={avatar} alt="User avatar" width="48" />
+          <p class="name">{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
