b127fd5172d6093f0021aacdcc6959d374ca9f42
Vasyl Horaichuk <vg.createam@gmail.com>
2023-01-05 14:25:47 +0200

    Оновлення

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
-
 // import data from './Data.data.json';
 import { Profile } from './Profile/Profile';
+import { StatisticList } from './StatisticList/StatisticList';
+
 import user from './Data/user.json';
+import statistics from './Data/data.json';
 
 export const App = () => {
   return (
@@ -11,7 +13,9 @@ export const App = () => {
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
-        stats={user.stats}/>
+        stats={user.stats}
+      />
+      <StatisticList title="Upload stats" statistics={statistics} />
     </div>
   );
 };
---
+++ src/components/StatisticList/StatisticList.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+export const StatisticList = ({ statistics, title }) => {
+  return (
+    <section class="statistics">
+      {title.length > 0 && <h2 class="title">{title}</h2>}
+      <ul class="stat-list">
+        {statistics.map(({ id, label, percentage }) => (
+          <li class="item" key={id}>
+            <span class="label">{label}</span>
+            <span class="percentage">{percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+// StatisticList.propTypes = {
+//   statistics,
+// };
